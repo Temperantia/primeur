131ada697e8659611c9b92eeb8e9ef87afda695b
realToddler <emilien.jolivet@gmail.com>
2023-06-17 00:18:38 +0200

    cities done

--- cities.ts
+++ cities.ts
@@ -6,6 +6,7 @@ const filePath = './villes_france.csv';
 const readableStream = fs.createReadStream(filePath, 'utf-8');
 
 const cities: string[] = [];
+
 readableStream.pipe(csvParser())
     .on('data', (row: any) => {
         if (row['618'] > 5000) {
@@ -13,9 +14,15 @@ readableStream.pipe(csvParser())
         }
     })
     .on('end', () => {
-        console.log('CSV file parsed successfully.');
+        const jsonData = JSON.stringify(cities, null, 2);
+        fs.writeFile('cities.json', jsonData, 'utf-8', (err) => {
+            if (err) {
+                console.error('Error writing JSON file:', err);
+            } else {
+                console.log('JSON file written successfully.');
+            }
+        });
     })
     .on('error', (error: Error) => {
         console.error('Error parsing CSV file:', error);
     });
-console.log(cities);
