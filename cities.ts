672bdcbd8bf04fbcde404373008e0ffc26b6acb2
Alexandre Du Lorier <adulorier@gmail.com>
2023-06-17 12:23:21 +0200

    lead list

--- cities.ts
+++ cities.ts
@@ -1,28 +1,29 @@
-import fs from 'fs';
-import csvParser from 'csv-parser';
+import fs from "fs";
+import csvParser from "csv-parser";
 
-const filePath = './villes_france.csv';
+const filePath = "./villes_france.csv";
 
-const readableStream = fs.createReadStream(filePath, 'utf-8');
+const readableStream = fs.createReadStream(filePath, "utf-8");
 
 const cities: string[] = [];
 
-readableStream.pipe(csvParser())
-    .on('data', (row: any) => {
-        if (row['618'] > 5000) {
-            cities.push(row['Ozan']);
-        }
-    })
-    .on('end', () => {
-        const jsonData = JSON.stringify(cities, null, 2);
-        fs.writeFile('cities.json', jsonData, 'utf-8', (err) => {
-            if (err) {
-                console.error('Error writing JSON file:', err);
-            } else {
-                console.log('JSON file written successfully.');
-            }
-        });
-    })
-    .on('error', (error: Error) => {
-        console.error('Error parsing CSV file:', error);
+readableStream
+  .pipe(csvParser())
+  .on("data", (row: any) => {
+    if (row["618"] > 5000) {
+      cities.push(row["Ozan"]);
+    }
+  })
+  .on("end", () => {
+    const jsonData = JSON.stringify(cities, null, 2);
+    fs.writeFile("cities.json", jsonData, "utf-8", (err) => {
+      if (err) {
+        console.error("Error writing JSON file:", err);
+      } else {
+        console.log("JSON file written successfully.");
+      }
     });
+  })
+  .on("error", (error: Error) => {
+    console.error("Error parsing CSV file:", error);
+  });
--- tripadvisor/crawler.ts
+++ tripadvisor/crawler.ts
@@ -1,8 +1,9 @@
 import { writeFileSync } from "fs";
 import puppeteer, { Page } from "puppeteer";
+
+import cities from "../cities.json";
 import { Lead } from "../types";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const MAILTO_REGEX = /mailto:([^\?]*)/;
 
 const scrapeCityPage = async (page: Page, city: string, start: string) => {
@@ -12,49 +13,105 @@ const scrapeCityPage = async (page: Page, city: string, start: string) => {
 
   await page.waitForSelector(".pageNum");
   const pages = (await page.$$(".pageNum")).length;
-  const urls = await page.$$eval(
-    ".content-column .result-content-columns",
-    (elements) =>
-      elements.map((element) =>
-        element.getAttribute("onclick")?.split(",")[3].replace(/'/g, "").trim()
-      )
-  );
-
-  const leads: Lead[] = [];
-  for (const url of urls) {
-    await page.goto(`https://www.tripadvisor.com${url}`);
-    const content = await page.content();
-    const email = content.match(MAILTO_REGEX)?.[0].split(":")[1];
-    const name = await page.$eval(
-      "[data-test-target='top-info-header']",
-      (element) => element.textContent
+  const restaurants = await page.$$(".content-column .result-content-columns");
+  const urls = [];
+  let lastPage = false;
+  let timesDifferent = 0;
+  for (const element of restaurants) {
+    const cityName = await element.evaluate((element) =>
+      element
+        .querySelector(".address-text")
+        ?.innerHTML.split(",")[1]
+        .trim()
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
     );
-    if (!email || !name) continue;
-    leads.push({ name, email, city });
+    if (
+      cityName !==
+      city
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+    ) {
+      ++timesDifferent;
+      if (timesDifferent > 2) {
+        lastPage = true;
+        break;
+      }
+    } else {
+      timesDifferent = 0;
+    }
+
+    const url = await element.evaluate((element) =>
+      element.getAttribute("onclick")?.split(",")[3].replace(/'/g, "").trim()
+    );
+
+    urls.push(url);
   }
 
-  return { pages, leads };
+  const leads: Lead[] = [];
+
+  for (const url of urls) {
+    console.log("visit " + url);
+    await page.goto(`https://www.tripadvisor.com${url}`);
+
+    try {
+      const content = await page.content();
+      const email = content.match(MAILTO_REGEX)?.[0].split(":")[1];
+      const name = await page.$eval(
+        "[data-test-target='top-info-header']",
+        (element) => element.textContent
+      );
+
+      if (!email || !name) continue;
+      leads.push({ name, email, city });
+    } catch (error) {
+      console.error("missing element in ", url);
+    }
+  }
+
+  return { pages: lastPage ? 0 : pages, leads };
 };
 
 const scrapeCity = async (page: Page, city: string) => {
-  const { pages, leads } = await scrapeCityPage(page, city, "0");
+  let { pages, leads } = await scrapeCityPage(page, city, "0");
   for (let i = 2; i <= pages; i++) {
-    const { leads: newLeads } = await scrapeCityPage(
+    let { leads: newLeads, pages: newPages } = await scrapeCityPage(
       page,
       city,
       ((i - 1) * 30).toString()
     );
+
     leads.push(...newLeads);
+    if (newPages !== pages) {
+      return leads;
+    }
   }
   return leads;
 };
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: "new" });
     const page = await browser.newPage();
-    const leads = await scrapeCity(page, "obernai");
-    writeFileSync("leads.json", JSON.stringify(leads, null, 2));
+
+    const leads: Lead[] = [];
+    let index = 0;
+    for (const city of cities) {
+      console.log("start ", city);
+      try {
+        const newLeads = await scrapeCity(page, city);
+        leads.push(...newLeads);
+        console.log(index, city, newLeads.length);
+      } catch (error) {
+        console.error(error);
+      }
+      ++index;
+      if (index % 50 === 0) {
+        writeFileSync("leads.json", JSON.stringify(leads, null, 2));
+      }
+    }
     await page.close();
     await browser.close();
   } catch (error) {
